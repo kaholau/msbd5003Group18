804325ddd2f28a2cdacb53cb3e3803e9c47faa76
kaho Lau <khlaukaho@gmail.com>
2017-11-18 17:31:20 +0800

    added plot for points and reachDis

--- optics_main.py
+++ optics_main.py
@@ -10,7 +10,7 @@ from pyspark.sql import Row
 
 source = [[15.0,16.0],[50.0,55.0]]
 numOfpt = 10
-deviationFromPoint = 10
+deviationFromPoint = 10.0
 
 def get_random_point():
 	points =[]
@@ -21,6 +21,7 @@ def get_random_point():
 	return points
 
 def plot_point(points):
+	print "The original points:\n",points
 	pts_x = np.array([x[0] for x in points])
 	pts_y = np.array([y[1] for y in points])
 	plt.scatter(pts_x, pts_y)
@@ -30,11 +31,13 @@ def plot_point(points):
 
 
 sc=SparkContext()
-random_points = get_random_point()
-rdd = sc.parallelize(random_points, 2).cache()
+#points = get_random_point()
+points =[[17.0, 21.0], [8.0, 25.0], [8.0, 18.0], [22.0, 23.0], [9.0, 25.0], [15.0, 7.0], [14.0, 10.0], [18.0, 13.0], [15.0, 20.0], [14.0, 7.0], [47.0, 46.0], [49.0, 63.0], [55.0, 53.0], [57.0, 49.0], [41.0, 46.0], [54.0, 47.0], [45.0, 60.0], [58.0, 46.0], [50.0, 57.0], [57.0, 64.0]]
+
+rdd = sc.parallelize(points, 2).cache()
 
 MIN_PTS_NUM = sc.broadcast(4)
-RADIUS = sc.broadcast(6)
+RADIUS = sc.broadcast(8)
 op = optics.OPTICS(MIN_PTS_NUM,RADIUS)
 
 # result is a rdd of Point Class Object sorted base of opticId
@@ -46,13 +49,8 @@ print result.take(10)
 
 
 
-
-
-
-
-'''
 #below is for plotting only
-plot_point(random_points)
+plot_point(points)
 plt.figure()
 
 op_rDis = result.map(lambda p: p.reachDis \
@@ -72,4 +70,3 @@ plt.bar(xrange(op_np.size), op_np)
 
 
 plt.show()
-'''
