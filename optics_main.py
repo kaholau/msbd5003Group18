13ee7528fdf8f6c0261ce43dc86e649565cbe971
kaho Lau <khlaukaho@gmail.com>
2017-11-19 17:57:29 +0800

    added random sample set generation support multiple centers

--- optics_main.py
+++ optics_main.py
@@ -7,13 +7,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 from pyspark.sql import Row
+from time import gmtime, strftime
 
 
-
-def get_random_point(source,numOfpt,deviationFromPoint):
+def get_random_point(source,numOfpt,dev):
 	points =[]
-	dev=float(deviationFromPoint)
-	if numOfpt>=100:
+	
+	if numOfpt>=50:
 		pts_x = sum([p[0] for p in source])
 		pts_y = sum([p[1] for p in source])
 		center_n = [pts_x/len(source),pts_y/len(source)]
@@ -23,10 +23,10 @@ def get_random_point(source,numOfpt,deviationFromPoint):
 		  	*int(random.random() * dev_n*3) for i in range(len(center_n))])
 		numOfpt = numOfpt*0.8
 
-	for pt in range(int(numOfpt/2)):
-		for center in source:
+	for pt in range(int(numOfpt/len(source))):
+		for i,center in enumerate(source):
 			  points.append([center[i] + (-1 if random.random()>0.5 else 1)\
-		  	*int(random.random() * dev) for i in range(len(center))])
+		  	*int(random.random() * dev[i]) for i in range(len(center))])
 	return points
 
 def plot_point(points,color):
@@ -37,23 +37,27 @@ def plot_point(points,color):
 	return
 
 
-
 sc=SparkContext()
 sc.setLogLevel("ERROR")
-source = [[25.0,30.0],[45.0,55.0]]
-numOfpt = 10
-deviationFromPoint = 10
+
+Testing_pt_centers = [[15.0,16.0],[45.0,55.0],[1.0,40.0],[40.0,5.0]]
+Testing_pt_centers_dev = [10.0,20.0,10.0,10.0]
+num_Of_Testing_pt = 50
+deviation_From_Testing_Center = max(Testing_pt_centers_dev)
 random.seed(11111111)
-points = get_random_point(source,numOfpt,deviationFromPoint)
+points = get_random_point(Testing_pt_centers,num_Of_Testing_pt,Testing_pt_centers_dev)
 #points =[[17.0, 21.0], [8.0, 25.0], [8.0, 18.0], [22.0, 23.0], [9.0, 25.0], [15.0, 7.0], [14.0, 10.0], [18.0, 13.0], [15.0, 20.0], [14.0, 7.0], [47.0, 46.0], [49.0, 63.0], [55.0, 53.0], [57.0, 49.0], [41.0, 46.0], [54.0, 47.0], [45.0, 60.0], [58.0, 46.0], [50.0, 57.0], [57.0, 64.0]]
 #this one gives good looking figures
 #points  = [[10.0, 21.0], [6.0, 8.0], [20.0, 14.0], [24.0, 16.0], [12.0, 7.0], [7.0, 19.0], [18.0, 18.0], [6.0, 15.0], [21.0, 10.0], [15.0, 19.0], [22.0, 11.0], [7.0, 7.0], [18.0, 21.0], [10.0, 23.0], [10.0, 10.0], [12.0, 22.0], [10.0, 20.0], [19.0, 18.0], [16.0, 24.0], [22.0, 24.0], [22.0, 13.0], [14.0, 7.0], [24.0, 11.0], [24.0, 22.0], [22.0, 19.0], [11.0, 19.0], [17.0, 14.0], [6.0, 22.0], [6.0, 13.0], [19.0, 22.0], [14.0, 25.0], [7.0, 17.0], [13.0, 21.0], [9.0, 13.0], [7.0, 14.0], [13.0, 12.0], [20.0, 20.0], [20.0, 22.0], [12.0, 24.0], [13.0, 24.0], [10.0, 16.0], [22.0, 17.0], [24.0, 13.0], [15.0, 10.0], [15.0, 8.0], [24.0, 15.0], [18.0, 11.0], [9.0, 17.0], [11.0, 16.0], [7.0, 17.0], [48.0, 54.0], [58.0, 61.0], [48.0, 57.0], [50.0, 48.0], [45.0, 59.0], [41.0, 56.0], [50.0, 48.0], [49.0, 50.0], [46.0, 55.0], [41.0, 54.0], [43.0, 64.0], [51.0, 57.0], [52.0, 54.0], [50.0, 48.0], [52.0, 62.0], [53.0, 57.0], [51.0, 60.0], [46.0, 55.0], [51.0, 51.0], [57.0, 58.0], [49.0, 50.0], [44.0, 48.0], [48.0, 60.0], [50.0, 62.0], [44.0, 53.0], [41.0, 54.0], [58.0, 57.0], [49.0, 60.0], [49.0, 54.0], [58.0, 54.0], [56.0, 61.0], [53.0, 61.0], [44.0, 51.0], [56.0, 53.0], [50.0, 51.0], [48.0, 64.0], [41.0, 55.0], [49.0, 64.0], [50.0, 53.0], [42.0, 50.0], [55.0, 64.0], [50.0, 49.0], [49.0, 57.0], [44.0, 54.0], [59.0, 63.0], [51.0, 61.0], [46.0, 54.0], [59.0, 55.0], [48.0, 62.0], [54.0, 47.0]]
 
+MIN_PTS_NUM = 4
+RADIUS = 20
+
 rdd = sc.parallelize(points, 2).cache()
 print "\nThe original points:\n",points
-MIN_PTS_NUM = sc.broadcast(4)
-RADIUS = sc.broadcast(9)
-op = optics.OPTICS(MIN_PTS_NUM,RADIUS)
+min_pts_num = sc.broadcast(MIN_PTS_NUM)
+radius = sc.broadcast(RADIUS)
+op = optics.OPTICS(min_pts_num,radius)
 
 # result is a rdd of Point Class Object sorted base of opticId
 result  = op.run(rdd)
@@ -64,8 +68,8 @@ flag, lb = op.getCluter(result, 9)
 #below is for plotting only
 pt_new = []
 pt_c = []
-with open("./optics_result.csv","w") as rfile:
-	rfile.write("Pid,OPTid,Px,Py,rD,cD,cluster,isCore\n")
+with open("./figure/optics_result_{}.csv".format(strftime("%Y%m%d%H%M%S", gmtime())),"w") as rfile:
+	rfile.write("Pid,OPTid,Px,Py,rD,cD,cluster,notCore\n")
 	for p in lb:
 		rfile.write("{},{},{},{},{},{},{},{}\n"\
 			.format(p.id,p.opticsId,points[p.id][0],points[p.id][1],p.reachDis,p.coreDis,p.flag,p.notCore))
@@ -80,10 +84,10 @@ pt_c = [c/color_n if c <99 else 1 for c in pt_c ]
 print pt_new
 print pt_c
 plot_point(pt_new,pt_c)
-plt.figure()
+
 
 op_rDis = result.map(lambda p: p.reachDis \
-				if p.reachDis <float(deviationFromPoint)*2 else float(deviationFromPoint)*2)
+				if p.reachDis <float(deviation_From_Testing_Center)*2 else float(deviation_From_Testing_Center)*2)
 #print op_rDis.take(10)
 sqlc = SQLContext(sc)
 row = Row("dis")
@@ -92,6 +96,8 @@ op_df = op_rDis.map(row).toDF()
 #print op_df.head(10)
 op_np = op_df.toPandas().values.reshape(-1)#np.array(op_df.select('*'))
 print "\nSome reachable distance:\n",op_np
-
+plt.savefig("./figure/figure_m{}_e{}_s{}_1_{}".format(MIN_PTS_NUM,RADIUS,num_Of_Testing_pt,strftime("%Y%m%d%H%M%S", gmtime())))
+plt.figure()
 plt.bar(xrange(op_np.size), op_np)
+plt.savefig("./figure/figure_m{}_e{}_s{}_2_{}".format(MIN_PTS_NUM,RADIUS,num_Of_Testing_pt,strftime("%Y%m%d%H%M%S", gmtime())))
 plt.show()
